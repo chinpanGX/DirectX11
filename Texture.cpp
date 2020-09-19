f11fffc1093cc869cfdf2dedcd6497202c6ddcf5
ChinpanGX <xchuhe@gameil.com>
2020-09-19 22:14:13 +0900

    Textureクラスを使いやすいように修正。２D描画用にSpriteクラスを追加。２D描画の確認済み

--- Texture.cpp
+++ Texture.cpp
@@ -7,6 +7,12 @@
 #include "Texture.h"
 #include "Renderer.h"
 
+#pragma region Define_Texture_Func
+Texture::Texture()
+{
+	m_Texture[256] = NULL;
+}
+
 // ロード
 unsigned int Texture::LoadTexture(const char * FileName)
 {
@@ -22,8 +28,60 @@ void Texture::Unload(unsigned int Texture)
 	m_Texture[Texture]->Release();
 }
 
-// 描画
-void Texture::Draw(unsigned int Texture, ID3D11Buffer * VertexBuffer, D3DXVECTOR2 DrawPosition, D3DXVECTOR2 DrawSize, D3DXVECTOR2 TexUpLeft, D3DXVECTOR2 TexDownRight)
+// セット
+ID3D11ShaderResourceView* Texture::SetTexture(unsigned int Texture)
+{
+	return m_Texture[Texture];
+}
+#pragma endregion Textureクラスの関数定義
+
+#pragma region Define_Sprite_Func
+void Sprite::Init()
+{
+	VERTEX_3D vertex[4];
+	{
+		vertex[0].Position = D3DXVECTOR3(-1.0f, 1.0f, 0.0f);
+		vertex[0].Normal = D3DXVECTOR3(0.0f, 1.0f, 0.0f);
+		vertex[0].Diffuse = D3DXVECTOR4(1.0f, 1.0f, 1.0f, 1.0f);
+		vertex[0].TexCoord = D3DXVECTOR2(0.0f, 0.0f);
+
+		vertex[1].Position = D3DXVECTOR3(1.0f, 1.0f, 0.0f);
+		vertex[1].Normal = D3DXVECTOR3(0.0f, 1.0f, 0.0f);
+		vertex[1].Diffuse = D3DXVECTOR4(1.0f, 1.0f, 1.0f, 1.0f);
+		vertex[1].TexCoord = D3DXVECTOR2(10.0f, 0.0f);
+
+		vertex[2].Position = D3DXVECTOR3(-1.0f, -1.0f, 0.0f);
+		vertex[2].Normal = D3DXVECTOR3(0.0f, 1.0f, 0.0f);
+		vertex[2].Diffuse = D3DXVECTOR4(1.0f, 1.0f, 1.0f, 1.0f);
+		vertex[2].TexCoord = D3DXVECTOR2(0.0f, 10.0f);
+
+		vertex[3].Position = D3DXVECTOR3(1.0f, -1.0f, 0.0f);
+		vertex[3].Normal = D3DXVECTOR3(0.0f, 1.0f, 0.0f);
+		vertex[3].Diffuse = D3DXVECTOR4(1.0f, 1.0f, 1.0f, 1.0f);
+		vertex[3].TexCoord = D3DXVECTOR2(10.0f, 10.0f);
+	}
+
+	//頂点バッファ生成
+	D3D11_BUFFER_DESC bd;
+	ZeroMemory(&bd, sizeof(bd));
+	bd.Usage = D3D11_USAGE_DYNAMIC;
+	bd.ByteWidth = sizeof(VERTEX_3D) * 4;
+	bd.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+	bd.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+
+	D3D11_SUBRESOURCE_DATA sd;
+	ZeroMemory(&sd, sizeof(sd));
+	sd.pSysMem = vertex;
+
+	Renderer::GetDevice()->CreateBuffer(&bd, &sd, &m_VtxBuffer);
+}
+
+void Sprite::Uninit()
+{
+	m_VtxBuffer->Release();
+}
+
+void Sprite::Draw(ID3D11ShaderResourceView * texture, D3DXVECTOR2 drawPosition, D3DXVECTOR2 drawSize, D3DXVECTOR2 texUpLeft, D3DXVECTOR2 texDownRight, D3DXCOLOR color)
 {
 	//ライト無効
 	LIGHT light;
@@ -31,48 +89,58 @@ void Texture::Draw(unsigned int Texture, ID3D11Buffer * VertexBuffer, D3DXVECTOR
 	Renderer::SetLight(light);
 
 	D3D11_MAPPED_SUBRESOURCE msr;
-	Renderer::GetDeviceContext()->Map(VertexBuffer, 0, D3D11_MAP_WRITE_DISCARD, 0, &msr);
+	Renderer::GetDeviceContext()->Map(m_VtxBuffer, 0, D3D11_MAP_WRITE_DISCARD, 0, &msr);
 
-	VERTEX_3D* vtx = (VERTEX_3D*)msr.pData;
-	vtx[0].Position = D3DXVECTOR3(DrawPosition.x -DrawSize.x * 0.5, DrawPosition.y - DrawSize.y * 0.5f,0.0f);
-	vtx[0].Normal = D3DXVECTOR3(0.0f, 1.0f, 0.0f);
-	vtx[0].Diffuse = D3DXVECTOR4(1.0f, 1.0f, 1.0f, 1.0f);
-	vtx[0].TexCoord = TexUpLeft;
+	VERTEX_3D* vertex = (VERTEX_3D*)msr.pData;
 
-	vtx[1].Position = D3DXVECTOR3(DrawPosition.x + DrawSize.x * 0.5, DrawPosition.y - DrawSize.y * 0.5f, 0.0f);
-	vtx[1].Normal = D3DXVECTOR3(0.0f, 1.0f, 0.0f);
-	vtx[1].Diffuse = D3DXVECTOR4(1.0f, 1.0f, 1.0f, 1.0f);
-	vtx[1].TexCoord = D3DXVECTOR2(TexDownRight.x, TexUpLeft.y);
+	vertex[0].Position = D3DXVECTOR3(drawPosition.x - drawSize.x * 0.5f, drawPosition.y - drawSize.y * 0.5f, 0.0f);
+	vertex[0].Normal = D3DXVECTOR3(0.0f, 1.0f, 0.0f);
+	vertex[0].Diffuse = D3DXVECTOR4(1.0f, 1.0f, 1.0f, 1.0f);
+	vertex[0].TexCoord = texUpLeft;
 
-	vtx[2].Position = D3DXVECTOR3(DrawPosition.x - DrawSize.x * 0.5, DrawPosition.y + DrawSize.y * 0.5f, 0.0f);
-	vtx[2].Normal = D3DXVECTOR3(0.0f, 1.0f, 0.0f);
-	vtx[2].Diffuse = D3DXVECTOR4(1.0f, 1.0f, 1.0f, 1.0f);
-	vtx[2].TexCoord = D3DXVECTOR2(TexUpLeft.x, TexDownRight.y);
+	vertex[1].Position = D3DXVECTOR3(drawPosition.x + drawSize.x * 0.5f, drawPosition.y - drawSize.y * 0.5f, 0.0f);
+	vertex[1].Normal = D3DXVECTOR3(0.0f, 1.0f, 0.0f);
+	vertex[1].Diffuse = D3DXVECTOR4(1.0f, 1.0f, 1.0f, 1.0f);
+	vertex[1].TexCoord = D3DXVECTOR2(texDownRight.x, texUpLeft.y);
 
-	vtx[3].Position = D3DXVECTOR3(DrawPosition.x - DrawSize.x * 0.5, DrawPosition.y - DrawSize.y * 0.5f, 0.0f);
-	vtx[3].Normal = D3DXVECTOR3(0.0f, 1.0f, 0.0f);
-	vtx[3].Diffuse = D3DXVECTOR4(1.0f, 1.0f, 1.0f, 1.0f);
-	vtx[3].TexCoord = TexDownRight;
+	vertex[2].Position = D3DXVECTOR3(drawPosition.x - drawSize.x * 0.5f, drawPosition.y + drawSize.y * 0.5f, 0.0f);
+	vertex[2].Normal = D3DXVECTOR3(0.0f, 1.0f, 0.0f);
+	vertex[2].Diffuse = D3DXVECTOR4(1.0f, 1.0f, 1.0f, 1.0f);
+	vertex[2].TexCoord = D3DXVECTOR2(texUpLeft.x, texDownRight.y);
 
-	Renderer::GetDeviceContext()->Unmap(VertexBuffer, 0);
+	vertex[3].Position = D3DXVECTOR3(drawPosition.x + drawSize.x * 0.5f, drawPosition.y + drawSize.y * 0.5f, 0.0f);
+	vertex[3].Normal = D3DXVECTOR3(0.0f, 1.0f, 0.0f);
+	vertex[3].Diffuse = D3DXVECTOR4(1.0f, 1.0f, 1.0f, 1.0f);
+	vertex[3].TexCoord = texDownRight;
 
-	// マトリクス設定
+	Renderer::GetDeviceContext()->Unmap(m_VtxBuffer, 0);
+
+	//マトリクス設定
 	Renderer::SetWorldViewProjection2D();
-	// 頂点バッファ設定
+
+	//頂点バッファ設定
 	UINT stride = sizeof(VERTEX_3D);
 	UINT offset = 0;
-	Renderer::GetDeviceContext()->IASetVertexBuffers(0, 1, &VertexBuffer, &stride, &offset);
-	// マテリアル設定
+	Renderer::GetDeviceContext()->IASetVertexBuffers(0, 1, &m_VtxBuffer, &stride, &offset);
+
+	//マテリアル設定
 	MATERIAL material;
 	ZeroMemory(&material, sizeof(material));
-	material.Diffuse = D3DXCOLOR(1.0f, 1.0f, 1.0f, m_Alpha);
+	material.Diffuse = color;
 	Renderer::SetMaterial(material);
-	// テクスチャ設定
-	Renderer::GetDeviceContext()->PSGetShaderResources(0, 1, &m_Texture[Texture]);
-	// プリミティブトポロジ設定
-	Renderer::GetDeviceContext()->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLESTRIP);
-	// ポリゴン描画
+
+	//テクスチャ設定
+	Renderer::GetDeviceContext()->PSSetShaderResources(0, 1, &texture);
+
+	//プリミティブトポロジ設定
+	Renderer::GetDeviceContext()->IASetPrimitiveTopology(
+		D3D11_PRIMITIVE_TOPOLOGY_TRIANGLESTRIP);
+
+	//ポリゴン描画
 	Renderer::GetDeviceContext()->Draw(4, 0);
+
 	//変更後は戻しておく
 	m_Alpha = 1.0f;
 }
+#pragma endregion Spriteクラスの関数定義
+
